fefe10fc60134ad384b26018b5af6734657c9782
coryrdunn <corydunn@mail.weber.edu>
2017-04-11 18:30:45 -0600

    Add Router and main Route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Router, Route, browserHistory} from 'react-router';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <ROUTER history={browserHistory}>
+    <ROUTE path='/' component={APP} />
+  </ROUTER>,
   document.getElementById('root')
 );
