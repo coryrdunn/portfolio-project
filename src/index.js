ac6f79fad0ca14317ac6d34aa3f4b7ed379f6172
coryrdunn <corydunn@mail.weber.edu>
2017-04-19 17:29:31 -0600

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
 import './index.css';
 import About from './About';
 import Projects from './Projects';
@@ -9,7 +9,7 @@ import Resume from './Resume';
 import Contact from './Contact';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route exact path='/' component={App} />
       <Route exact path='/about' component={About} />
       <Route exact path='/projects' component={Projects} />
