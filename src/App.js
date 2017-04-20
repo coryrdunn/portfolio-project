76945ca77b229c83add413c2b605609a2abeaa7f
coryrdunn <corydunn@mail.weber.edu>
2017-04-19 18:14:56 -0600

    new homepage img

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <Nav />
         <div className="homePage">
-          <img className="homeImg" src="https://lh3.googleusercontent.com/P5mTu5H7ihe4jUyjYyLWVTggBpIztdqQbVXS_PkZpkZSa9KulIB8Yc5Na1dQsYUHpZy1Oy2WBP0t806VLiF6Bq-_oa5Q5EbF5p9hpvVLQX-qwfEIyEvYOYyztI4f5ST9lXvZkQLwqEYDOP63T6NY3nPNjb3a8TkcopfVpEEUUnN4AzA619Xv9xenx_DIEFguoxiPetsjgKWXZlJ6SUx7Vo5nILc4CF0CCIZY8R6pFZzgUBV9zj1J_0m1t5bWiLlrOTBjRJP_CGqklkat9aEEClU-VCkhpBcLFSs5AFvtwl2jGSMcble5_Iq8HQeO5GFKb2Khknn4uhQNUtiTMzzBYAN3YxD_4zXfzoh_vcyODG9vOa09crHlfiDkX0XqIBzyIcTmWIZwHdWXUgRIqrlimuLjasvX3J5d00CgI2SgWLkdsgZxLnU0OgputFnT3ALOLALnEnRMDP9aKCuzP-GGn1KpAxXZGX-uov9Ddap6Amc4PexLFuLpwhEPSji5Ta-5vV0DR9R5xrvYN2oimz_HfgJIhusRsDP3iCB1OWxzQ2VisqcmfFcZMbJ_xLvEuOiqcpbU6wXROuWwcT8ZB8Myvge1N25ZROqq3CTbmSjlCKYVnYkT=w360-h203-no" alt="mainImg" />
+          <img className="homeImg" src="https://lh3.googleusercontent.com/LgfCYJ5G0G2Dm1uSZKE-5zu6HMtXGDJu7njc6vf3y6OWS-A9Ekm1ICKOiWGmsLnSUuvLdif1Z1TGxVxlmBW058r6s9gJD_x-ImFd-gcGV2-WlPzTyd3lqTwCKfdFPGzosCAQ5aPv9vNId3zfrXoHXQ1jmncC764oOP8VWZ2uCumrbPJBfenbbPfpbGDjxFIkiyv6LcXYmQVT826nYYerNwFu3GT0SM2h3sz8l-lD9mW0H1CnnE6qNT_xodHhzgf-OBMCeYjirC8p9k7JXSO-9TSupyUiT9dFrZqPDiGhGrhEiZqqVKS0OPkEYcjreG6qWZEIyAS_WcKJcRQTWAmhTOmgGbgSuztNtPsljE_cIcxee5dw3FPBivgKL3bHBJadlxpWs_XaExb1IidvaDwITRUC_A68qv1jxMHwF_z2d5xZPI5eNA_MIKgWmtDUuk5tSMKdrXvdudGEzFArPqs4_YxmPoSFlQ9i7cUUoBwaj-b1mNg6uGLy3VPCr0ZpXStJqPj9_0seGxk7kJQxpK6o4CSPuTbbQ-3u2prcPw8C6oDaHpLm_NEpBFaUcvneR2LaM7mrnZ32vsBwaPf1fDzosnPvZB9vu3qu6cHfcKMpWv2Zn08iYUIE=w2232-h1256-no" alt="mainImg" />
           <div className="homeTitle">Welcome To My Portfolio</div>
           <div className="homeDetails">
             A place to showcase projects, skills, and contact information.
